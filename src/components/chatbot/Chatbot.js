44b56df3608ad379dc5508eb783faf8cae8bdfd7
kaylynnkk <kaylynn.khem@student.csulb.edu>
2023-03-13 23:55:14 -0700

    implement chat box

--- src/components/chatbot/Chatbot.js
+++ src/components/chatbot/Chatbot.js
@@ -1,36 +1,87 @@
 import "./Chatbot.css";
 
-import HumanIcon from './static/HumanIcon.svg'
-import RobotIcon from './static/RobotIcon.svg'
+import HumanIcon from "./static/HumanIcon.svg";
+import RobotIcon from "./static/RobotIcon.svg";
+
+import { useState } from "react";
+
+import { OpenAI } from "./OpenAI/openai";
 
 const Chatbot = () => {
+  const [textInput, setTextInput] = useState("");
+  const [result, setResult] = useState("");
+
+  const handleInputChange = (event) => {
+    setTextInput(event.target.value);
+  };
+
+  async function handleClick(event) {
+    event.preventDefault();
+    console.log(textInput);
+
+    const userTextBox = document.getElementsByClassName("user-text")[0];
+
+    if (window.getComputedStyle(userTextBox).opacity === "0") {
+      userTextBox.style.opacity = 1;
+    }
+
+    const userQuestion = document.getElementById("user-question");
+    userQuestion.textContent = `Recommend me a ${textInput}`;
+
+    OpenAI(textInput).then((data) => {
+      console.log(data);
+      const res = data.result.slice(2, data.result.length);
+      console.log(res);
+
+      const botTextBox = document.getElementsByClassName("bot-text")[0];
+
+      if (window.getComputedStyle(botTextBox).opacity === "0") {
+        botTextBox.style.opacity = 1;
+      }
+
+      const botAnswer = document.getElementById("bot-answer");
+      botAnswer.textContent = `${res}`;
+    });
+  }
+
+  const UserText = () => {
+    return (
+      <div className="text user-text">
+        <div className="text-box">
+          <p id="user-question">Question</p>
+        </div>
+        <img src={HumanIcon} />
+      </div>
+    );
+  };
+
+  const BotText = () => {
+    return (
+      <div className="text bot-text">
+        <img src={RobotIcon} />
+        <div className="text-box">
+          <p id="bot-answer">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit
+          </p>
+        </div>
+      </div>
+    );
+  };
+
   return (
     <main>
       <div className="chat-window">
         <div className="chat">
-          
-          <div class="text user-text">
-              <div className="text-box">
-                <p>Question</p>
-              </div>
-              <img src={HumanIcon}/>
-          </div>
-          <div class="text bot-text">
-              <img src={RobotIcon}/>
-              <div className="text-box">
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
-              </div>
-          </div>
-          <div class="text user-text">
-              <div className="text-box">
-                <p>Question</p>
-              </div>
-              <img src={HumanIcon}/>
-          </div>
+          <UserText />
+          <BotText />
         </div>
         <div className="search-bar">
-          <button className="arrow-button"></button>
-          <input type="text" placeholder="Type something..."></input>
+          <button className="arrow-button" onClick={handleClick}></button>
+          <input
+            type="text"
+            placeholder="Recommend me a..."
+            onChange={handleInputChange}
+          ></input>
         </div>
       </div>
     </main>
---
+++ src/components/chatbot/OpenAI/openai.js
@@ -0,0 +1,36 @@
+import { Configuration, OpenAIApi } from "openai";
+
+const OPENAI_API_KEY = "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"
+
+const configuration = new Configuration({
+  apiKey: OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+async function OpenAI(text) {
+
+  try {
+    const completion = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: generatePrompt(text),
+      temperature: 0.6,
+    });
+    console.log(completion.data)
+    return { result: completion.data.choices[0].text };
+  } catch(error) {
+    // Consider adjusting the error handling logic for your use case
+    if (error.response) {
+      console.error(error.response.status, error.response.data);
+      
+    } else {
+      console.error(`Error with OpenAI API request: ${error.message}`);
+      
+    }
+  }
+}
+
+function generatePrompt(text) {
+  return `Recommend me a ${text}`;
+}
+
+export { OpenAI}
